b1f8e3959259a6b20f9604ddf4174ff50112fae5
Mateo Sierra <m.sierra1037@hotmail.com>
2024-02-02 19:40:10 -0500

    feature: main section

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Cardio Exercises</h1>
-            <h4>
+        <section>
+            <h1 className='hero-title'>Cardio <br /> Exercises</h1>
+            <h4 className='text-dim'>
                 Boost your fitness with quick cardio bursts! From HIIT to dance, we've got your pulse covered. Join us for a vibrant, healthier you! 💪🔥</h4>
+            <a href="" className='btn green-background'> get started</a>    
+            <a href="" className='btn'>preview</a>
 
-            <a href="" className='hover:no-underline'> get started</a>
-            <a href="">preview</a>
-        </div>
+            <div>
+                <a href="" className='btn'>38:14 <br /> Time</a>
+                <a href="" className='btn'>165 <br /> Calories</a>
+            </div>
+        </section>
     )
 }
 
