54f6fd425c37fa36f28f1eb280d4eea59bb4576a
Mateo Sierra <m.sierra1037@hotmail.com>
2024-02-04 20:59:39 -0500

    task: bento menu markup

--- app/Components/BentoMenu.jsx
+++ app/Components/BentoMenu.jsx
@@ -2,9 +2,64 @@ import React from 'react'
 
 const BentoMenu = () => {
 
-    
+  const workouts = [
+    {
+      name: 'Treadmill',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+    },
+    {
+      name: 'Yoga',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+    },
+    {
+      name: 'Weight Lifting',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+    },
+    {
+      name: 'Samba',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+    },
+    {
+      name: 'Kickboxing',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+    },
+  ]
+
+  const WorkoutCard = ({
+    classname,
+    imgSrc,
+    alt = "no alternative text was provided",
+    title,
+    caloriesBurnt,
+    workoutTime
+  }) => {
+    return (
+    <article className={classname}>
+      <figure>
+        <img src={imgSrc} alt={alt} />
+      </figure>
+      <div>
+        <h2>{title}</h2>
+        <p>{workoutTime}</p>
+        <p>{caloriesBurnt}</p>
+      </div>
+    </article>)
+  }
+
   return (
-    <div>BentoMenu</div>
+    <section class="grid md:grid-cols-3 grid-cols-2 gap-4 mt-10">
+      {workouts.map(({caloriesBurnt,name, workoutTime},index)=>(WorkoutCard({
+          title:name, 
+          caloriesBurnt,
+          workoutTime, 
+          alt: '',
+          classname: index > 0 ? 'p-4 bg-green-800' : 'p-4 bg-green-800 col-span-2 md:col-span-2' })))}
+    </section>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "./Components/Hero";
+import BentoMenu from "./Components/BentoMenu";
 export default function Home() {
   return (
 <main>
   <Hero></Hero>
+  <BentoMenu></BentoMenu>
 </main>
   );
 }
