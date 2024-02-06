335110ceba2df87e9916302b74289f3e2395bbaf
Mateo Sierra <m.sierra1037@hotmail.com>
2024-02-05 22:26:02 -0500

    feature: finished bento grid

--- app/Components/BentoMenu.jsx
+++ app/Components/BentoMenu.jsx
@@ -5,17 +5,18 @@ const BentoMenu = () => {
 
   const workouts = [
     {
-      workoutName: 'Treadmill',
-      workoutTime : '00:00',
-      caloriesBurnt : '100',
-      imgSrc : '/images/treadmill.jpg'
-    },
-    {
       workoutName: 'Yoga',
       workoutTime : '00:00',
       caloriesBurnt : '100',
       imgSrc : '/images/yoga.jpg'
     },
+    {
+      workoutName: 'Treadmill',
+      workoutTime : '00:00',
+      caloriesBurnt : '100',
+      imgSrc : '/images/treadmill.jpg'
+    },
+
     {
       workoutName: 'Weight Lifting',
       workoutTime : '00:00',
@@ -45,15 +46,17 @@ const BentoMenu = () => {
     workoutTime
   }) => {
     return (
-    <article className={classname}>
-      <figure>
-        <Image width={300} height={300} src={imgSrc} ></Image>
+    <article  className={` ${classname}`}>
+
+      <figure className='h-full'>
+        <Image className='object-cover w-full h-full' width={300} height={300} src={imgSrc} alt={alt} />
       </figure>
-      <div>
+      <div className='relative bottom-20 left-3 h-0 z-1'>
         <h2>{workoutName}</h2>
         <p>{workoutTime}</p>
         <p>{caloriesBurnt}</p>
       </div>
+      <div class="relative bg-black opacity-35  h-full bottom-full hover:opacity-10 cursor-pointer "></div>
     </article>)
   }
 
@@ -65,7 +68,7 @@ const BentoMenu = () => {
           caloriesBurnt,
           workoutTime, 
           alt: '',
-          classname: index > 0 ? 'p-4 bg-green-800' : 'p-4 bg-green-800 col-span-2 md:col-span-2' })))}
+          classname: index > 0 ? '' : 'col-span-2 md:col-span-2 h-96' })))}
     </section>
   )
 }
